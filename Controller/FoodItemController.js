cc5c8227e0d0ea25e832460e5d5fc10c7bf4fb97
kabita <kabita.eminence@gmail.com>
2022-04-27 11:43:19 +0530

    files updated

---
+++ Controller/FoodItemController.js
@@ -0,0 +1,55 @@
+const foodData = require("../Models/FoodItemModels");
+
+const create = async (req, res) => {
+  try {
+    const { foodName, price, type, Image } = req.body;
+
+    const obj = {
+      foodName,
+      price,
+      type,
+      Image,
+    };
+
+    const data = await foodData.create(obj);
+    //   data.save();
+    return res.status(200).json({ message: "success", data: data });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "something went wrong", error: error });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const data = await foodData.find();
+    return res.status(200).json({ message: "success", data: data });
+  } catch (error) {
+    return res.status(400).json({ message: "somthing wrong", error: error });
+  }
+};
+
+const patch = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const data = await foodData.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    return res.status(200).json({ message: "success", data: data });
+  } catch (e) {
+    return res.status(500).json({ message: "somthing went wrong", e: e });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const data = await foodData.deleteOne({ _id });
+    return res.status(200).json({ message: "success", data: data });
+  } catch (err) {
+    return res.status(500).json({ message: "somthing went wrong", err: err });
+  }
+};
+
+module.exports = { create, get, patch, remove };
--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var multer = require('multer')
-var fs = require('fs')
-var path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var fs = require("fs");
+var path = require("path");
 
-const cors = require('cors');
+const cors = require("cors");
 var app = express();
-const Routes = require('./Routes/UserRoute')
+const Routes = require("./Routes/UserRoute");
+const resturant_Routes = require("./Routes/ResturantRoutes");
+
+const food_routes = require("./Routes/FoodItemRoutes");
 
 const Router = express.Router();
-mongoose.connect('mongodb://localhost:27017/zomato');
+mongoose.connect("mongodb://localhost:27017/zomato");
 mongoose.Promise = global.Promise;
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/',Routes);
+app.use("/", Routes);
 
-app.use((req,res,next) => { 
-  next()
+app.use("/resturant", resturant_Routes);
+app.use("/fooditem", food_routes);
+
+app.use((req, res, next) => {
+  next();
 });
-    
-app.listen(process.env.port || 5000,function() {
-console.log('welcome!')
-})
+
+app.listen(process.env.port || 5000, function () {
+  console.log("welcome!");
+});
